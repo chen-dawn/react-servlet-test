8e2bdb15c793c4e3211216ed32e4d9454a037529
Dawn Chen <dawnxchen@google.com>
2020-06-19 00:14:22 -0700

    test2

---
+++ target/appengine-staging/precache-manifest.7cfbfd9328b897c53ec420c0a96a73be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db678cf946ac56c8cfc7f3d5f6efc333",
+    "url": "http://carolynlwang-step.uc.r.appspot.com/dawn-app/index.html"
+  },
+  {
+    "revision": "03da338785e1586adf37",
+    "url": "http://carolynlwang-step.uc.r.appspot.com/dawn-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cf8eb051cc22761850bc",
+    "url": "http://carolynlwang-step.uc.r.appspot.com/dawn-app/static/js/2.50d3b0f5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "http://carolynlwang-step.uc.r.appspot.com/dawn-app/static/js/2.50d3b0f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03da338785e1586adf37",
+    "url": "http://carolynlwang-step.uc.r.appspot.com/dawn-app/static/js/main.83c9e421.chunk.js"
+  },
+  {
+    "revision": "bac5945d19cf68e98eb5",
+    "url": "http://carolynlwang-step.uc.r.appspot.com/dawn-app/static/js/runtime-main.dc78d3d2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "http://carolynlwang-step.uc.r.appspot.com/dawn-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
